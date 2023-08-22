0b10d159848b4b641384547e6d39ada7b1cc9f72
Fahim4u <fahim4u.95@gmail.com>
2023-08-22 18:07:31 +0530

    Updated
    Assignment

--- CharacterIdentifier/src/Main.java
+++ CharacterIdentifier/src/Main.java
@@ -2,37 +2,57 @@ import java.util.Scanner;
 public class Main {
 	public static void identifyCharacter(char ch)
 	{
-		if (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
-		{
-			System.out.println("Lower-case vowel");
-		}
-		else if (ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U')
-		{
-			System.out.println("Upper-case vowel");
-		}
-		else if (ch>='a' && ch<='z')
-		{
-			System.out.println("Lower-case consonant");
-		}
-		else if (ch>='A' && ch<='Z')
-		{
-			System.out.println("Upper-case consonant");
-		}
-		else if (ch>='0' && ch<='9')
-		{
-			System.out.println("Digit");
-		}
-		else
-		{
-			System.out.println("Special character");
-		}
-	}
-	public static void main(String[] args)
+
+	// convert the character to a Character object
+	Character c = new Character(ch);
+	// check if the character is a letter
+	if (c.isLetter(ch))
 	{
+	// check if the character is lower case
+	if (c.isLowerCase(ch))
+	{
+	// check if the character is a vowel
+	if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+	{
+	System.out.println("Lower-case vowel");
+	}
+	else
+	{
+	System.out.println("Lower-case consonant");
+	}
+	}
+	// check if the character is upper case
+	else if (c.isUpperCase(ch))
+	{
+	// check if the character is a vowel
+	if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+	{
+	System.out.println("Upper-case vowel");
+	}
+	else
+	{
+	System.out.println("Upper-case consonant");
+	}
+	}
+	}
+	// check if the character is a digit
+	else if (c.isDigit(ch))
+	{
+	System.out.println("Digit");
+	}
+	// otherwise, the character is a special character
+	else
+	{
+	System.out.println("Special character");
+	}
+	}
+
+	public static void main(String[] args) {
+
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter any character");
+		System.out.println("enter an ch ");
 		char ch = scan.next().charAt(0);
-		identifyCharacter(ch);
 		scan.close();
+		identifyCharacter(ch);
 	}
 }
